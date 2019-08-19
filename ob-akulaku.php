aa50f178e212d969e2be31f7c222dfeb2dea5425
Hilaludin Wahid <hilaludinwahid@gmail.com>
2019-08-19 15:49:45 +0700

    wp plugin #1
    Co-Authored-By: Hilaludin Wahid <hylal@users.noreply.github.com>

---
+++ ob-akulaku.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: OB Akulaku
+Plugin URI: https://www.ob-fit.com
+Description: Payment Gateway untuk OB Fit dari Akulaku
+Version: 0.1.0
+Author: Hilaludin Wahid
+Author URI: https://www.wahid.biz
+License: GPL-3.0+
+License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+WC tested up to: 3.6.5
+ */
+
+ if (!define('ASBPATH')) {
+     die;
+ }
