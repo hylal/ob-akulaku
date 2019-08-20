218bbf7d7769b2a35bb5f40c09edb65a3adb5533
Hilaludin Wahid <hilaludinwahid@gmail.com>
2019-08-20 14:16:26 +0700

    Mendapatkan data nilai settings

--- ob-akulaku.php
+++ ob-akulaku.php
@@ -24,5 +24,49 @@ WC tested up to: 3.7
   }
 
   // memulai plugins
-  add_ction( 'plugin_loaded', 'OB_akulaku_payment_gateway' );
-  
+  add_ction( 'plugin_loaded', 'exe_OB_akulaku_payment_gateway' );
+  function exe_OB_akulaku_payment_gateway() {
+
+    // Menghubungi WooCommerce
+
+    function add_OB_akulaku_payment_gateway( $methods ) {
+      $methods[] = 'add_OB_akulaku_payment_gateway';
+      return $methods;
+    }
+
+    add_filter( 'woocommerce_payment_gateways', 'add_OB_akulaku_payment_gateway' );
+
+    if ( ! class_exist( 'WC_Payment_Gateway' ) )
+    return;
+
+    // OB akulaku class
+    class OB_akulaku_payment_gateway extends WC_Payment_Gateway {
+      // Constructor
+      public function __construct() {
+        $this->id = 'obakulaku';
+        $this->icon apply_filters( ' woocommerce_obakulaku_icon', plugins_url( 'public/image/logo-al.png', __FILE__ ) );
+        $this->has_fields = true;
+        $this->method_title = 'OB Akulaku';
+        $this->method_description = 'Integrasi Payment gateway ke akulaku.';
+
+        // load ke isian kolom
+        $this->init_form_fields();
+        // load settings
+        $this->init_settings():
+
+        // Mendapatkan data nilai settings
+        $this->title		 = $this->get_option( 'title' );
+        $this->description	 = $this->get_option( 'description' );
+        $this->enabled		 = $this->get_option( 'enabled' );
+        $this->sandbox		 = $this->get_option( 'sandbox' );
+        $this->environment	 = $this->sandbox == 'no' ? 'production' : 'sandbox';
+        $this->appId	 = $this->sandbox == 'no' ? $this->get_option( 'appId' ) : $this->get_option( 'sandbox_appId' );
+        $this->secKey	 = $this->sandbox == 'no' ? $this->get_option( 'secKey' ) : $this->get_option( 'sandbox_secKey' );
+        
+
+
+
+
+      }
+    }
+  }
